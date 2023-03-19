9eb48611506da7741f8c2119ffcd130d01f8138b
LMH <mhyhl2014@naver.com>
2023-03-19 17:05:07 +0900

    mistake commit

--- presentation/src/main/java/com/example/newswithcleanarchitecture/bottom_navigation/components/Navigation.kt
+++ presentation/src/main/java/com/example/newswithcleanarchitecture/bottom_navigation/components/Navigation.kt
@@ -2,8 +2,10 @@ package com.example.newswithcleanarchitecture.bottom_navigation.components
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.newswithcleanarchitecture.breaking_news.BreakingNewsScreen
 import com.example.newswithcleanarchitecture.favorite_news.FavoriteNewsScreen
 import com.example.newswithcleanarchitecture.search_news.SearchNewsScreen
@@ -20,5 +22,57 @@ fun Navigation(navController: NavHostController) {
         composable("searchnews") {
             SearchNewsScreen()
         }
+        composable(
+            route = "article" +
+                    "?articleId={articleId}&publishedAt={publishedAt}&sourceId={sourceId}&sourceName={sourceName}"+
+                    "&title={title}&url={url}&urlToImage={urlToImage}",
+            arguments = listOf(
+                navArgument(
+                    name = "articleId"
+                ) {
+                    type = NavType.IntType
+                    defaultValue = -1
+                },
+                navArgument(
+                    name = "publishedAt"
+                ) {
+                    type = NavType.StringType
+                    defaultValue = ""
+                },
+                navArgument(
+                    name = "sourceId"
+                ) {
+                    type = NavType.IntType
+                    defaultValue = -1
+                },
+                navArgument(
+                    name = "sourceName"
+                ) {
+                    type = NavType.StringType
+                    defaultValue = ""
+                },
+                navArgument(
+                    name = "title"
+                ) {
+                    type = NavType.StringType
+                    defaultValue = ""
+                },
+                navArgument(
+                    name = "url"
+                ) {
+                    type = NavType.StringType
+                    defaultValue = ""
+                },
+                navArgument(
+                    name = "urlToImage"
+                ) {
+                    type = NavType.StringType
+                    defaultValue = ""
+                }
+            )
+        ) {
+
+        }
+
     }
 }
