14104552145433d34d3d6505fdcad12259e8a2f8
LeeMyoungHan <mhyhl654@gmail.com>
2023-03-19 18:05:57 +0900

    check commit

--- presentation/src/main/java/com/example/newswithcleanarchitecture/MainActivity.kt
+++ presentation/src/main/java/com/example/newswithcleanarchitecture/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.newswithcleanarchitecture
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+
+
+
+
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
